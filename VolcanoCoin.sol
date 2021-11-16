5ffea213bd7ef26136e0da1e635517f7e084ba84
crdbc <93407683+crdbc@users.noreply.github.com>
2021-11-16 12:25:16 -0500

    Homework 4 changes

--- VolcanoCoin.sol
+++ VolcanoCoin.sol
@@ -4,13 +4,22 @@ pragma solidity ^0.8.0;
 
 contract VolcanoCoin {
     
+    struct Payment{
+        address recipient;
+        uint amount;
+    }
+    
     uint total_supply = 10000;
     address owner;
+    mapping(address => uint) balance;
+    mapping(address => Payment[]) payments;
     
     event supply_increase(uint);
+    event token_transfer(address, uint);
     
     constructor(){
         owner = msg.sender;
+        balance[owner] = total_supply;
     }
     
     modifier onlyOwner {
@@ -26,5 +35,14 @@ contract VolcanoCoin {
     function increaseTotalSupply() public onlyOwner {
         total_supply += 1000;
         emit supply_increase(total_supply);
+    }
+    
+    function getUserBalance(address _userAddress) public view returns (uint){
+        return balance[_userAddress];
+    }
+    
+    function transfer(address _transferToAddress, uint _amount) public {
+        payable(_transferToAddress).transfer(_amount);
+        emit token_transfer(_transferToAddress, _amount);
     }
 }
